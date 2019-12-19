c2ffd2c2c29e4c625d866c639b7a58c8eeb45bf7
greek <greekks@gmail.com>
2019-12-20 00:01:30 +0600

    some amendment was made

--- SecondTask.java
+++ SecondTask.java
@@ -3,7 +3,8 @@ package com.greekk.hometask1;
 public class SecondTask {
     public static void main(String[] args) {
         int  i = 60;
-        if(i >= 60 || i == 9){
+        boolean expr = i < 10 && i >= 60 || i == 9;
+        if(expr){
             System.out.println(i);
         }
     }
